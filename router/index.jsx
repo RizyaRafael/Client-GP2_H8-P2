f57cbc295329c1d665e77dcd4f5fa8189222e318
DimasAditya2 <esadimasadityaa@gmail.com>
2024-06-19 15:50:53 +0700

    feat: form for game page and sweatalert

--- router/index.jsx
+++ router/index.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../src/pages/HomePage";
+import GamePage from "../src/pages/GamePage";
 
 
 const router = createBrowserRouter([
@@ -7,6 +8,10 @@ const router = createBrowserRouter([
       path: "/",
       element: <HomePage/>,
     },
+    {
+      path: "/games",
+      element: <GamePage/>
+    }
 ]);
 
 
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2'
 export default function GamePage() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Game Page
-      </h1>
-    )
+  const [guess, setGuess] = useState('');
+  // const [message, setMessage] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (guess.toLowerCase() === 'denis') {
+      Swal.fire("Selamat jawaban anda benar !");
+    } else {
+      Swal.fire("Ya salah :)") 
+    }
+  };
+
+  return (
+    <div className="container-100 h-screen flex flex-col items-center justify-center bg-slate-700">
+      <h1 className="text-4xl text-white mb-8">Tebak Kata</h1>
+      <p className="text-xl text-white mb-5">Kata yang harus ditebak memiliki 5 huruf, namanya kayak alat kelamin depan diganti d</p>
+
+      <form 
+        className="lg:w-1/4 bg-slate-500 py-5 px-5 rounded-md md:w-2/4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-5">
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 px-4 dark:focus:border-blue-500"
+            placeholder="Masukkan tebakan"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+        </div>
+        <button
+        style={{width: '100%'}}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+
+    </div>
+  );
 }
