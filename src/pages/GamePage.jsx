7cba47e48506db3a3ccc2e5ba5932384933ddba9
Rizya Rafael <rizyarafaelbro@gmail.com>
2024-06-19 17:21:48 +0700

    feat: testing socket Rafael

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2'
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3000");
+
+
 export default function GamePage() {
   const [guess, setGuess] = useState('');
   // const [message, setMessage] = useState(null);
@@ -9,16 +13,25 @@ export default function GamePage() {
     if (guess.toLowerCase() === 'denis') {
       Swal.fire("Selamat jawaban anda benar !");
     } else {
-      Swal.fire("Ya salah :)") 
+      Swal.fire("Ya salah :)")
     }
   };
+
+  useEffect(() => {
+    socket.on("message", (inimessage) => {
+      console.log({ inimessage });
+    })
+    return () => {
+      socket.off('message')
+    }
+  }, [])
 
   return (
     <div className="container-100 h-screen flex flex-col items-center justify-center bg-slate-700">
       <h1 className="text-4xl text-white mb-8">Tebak Kata</h1>
       <p className="text-xl text-white mb-5">Kata yang harus ditebak memiliki 5 huruf, namanya kayak alat kelamin depan diganti d</p>
 
-      <form 
+      <form
         className="lg:w-1/4 bg-slate-500 py-5 px-5 rounded-md md:w-2/4"
         onSubmit={handleSubmit}
       >
@@ -32,7 +45,7 @@ export default function GamePage() {
           />
         </div>
         <button
-        style={{width: '100%'}}
+          style={{ width: '100%' }}
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
